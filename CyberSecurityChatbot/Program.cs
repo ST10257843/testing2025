97d037c4fe4c87eac8af76241e034af05c1b7dc4
ST10257843 <litiyana5@gmail.com>
2025-04-22 19:19:30 +0200

    Initial commit with project setup
    Developer information:
    ST10257843
    Litiyana Monique Mohanlall
    litiyana5@gmail.com
    
    Programming 2A	(PROG6221)
    POE
    Part 1: Basic Chatbot interaction with voice greeting and image

---
+++ CyberSecurityChatbot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CyberSecurityAwarenessBot
+{
+    /*
+     * Developer information:
+     * ST10257843
+     * Litiyana Monique Mohanlall
+     * litiyana5@gmail.com
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Cybersecurity Awareness Bot: CyberSavvy"; //Displays the name of the chatbox
+
+
+        }
+    }
+    /*
+    References and Code Contributions:
+    * Bell, D. and Parr, M. 2009. C# for students. Harlow: Pearson;
+    * Deitel, P.J. and Deitel H.M. Visual C# 2010: how to program. Upper Saddle River, (NJ): Pearson Prentice Hall.
+    * Doyle, B. 2016. C# programming: from problem analysis to program design. 5th ed. Boston, (MA): Cengage Learning. 
+    * ElevenLabs. 2025. Text to Speech – Voice Synthesis Platform. [online]. Available at: https://elevenlabs.io/app/speech-synthesis/text-to-speech [Accessed 16 April 2025].
+    * Farrell, J. 2011. Microsoft Visual C# 2010: an introduction to object-oriented programming. Mason, (OH): South-Western.
+    * Lindemulder, G. and Kosinski, M. 2024. What is cybersecurity?. [online]. Available at: https://www.ibm.com/think/topics/cybersecurity [Accessed 14 April 2025].
+    * Nakov, S. and Kolev, V. 2013. Fundamentals of Computer Programming with C#. Sofia: [ebook]. Available at: http://www.introprogramming.info [Accessed 16 April 2024].
+    * Patorjk. n.d. Text to ASCII art generator (TAAG). [online]. Available at: https://patorjk.com/software/taag/ [Accessed 10 April 2025].
+    * W3Schools. 2024. C# Tutorial. [online]. Available at: https://www.w3schools.com/cs/index.php [Accessed 10 April 2025].
+    */
+}
